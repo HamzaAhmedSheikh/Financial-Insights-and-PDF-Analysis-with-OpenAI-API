c8668d4c152c9be3897488f010750bb304dbda71
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-12-24 03:21:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ from openai.types.beta.thread import Thread
 def initialize_openai_client(api_key):
     return openai.OpenAI(api_key=api_key)
 
-
-
 # Load the API key from the .env file
 load_dotenv(find_dotenv())
 
