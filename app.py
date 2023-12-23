fc0ca1121b3732da99f24a87cb0b8e28660cb070
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-12-23 23:36:38 +0500

    I have incorporated the PDF Analyzer into our Streamlit app. Users can now submit PDF documents and obtain insights or particular information retrieved by the OpenAI Assistant.

--- app.py
+++ app.py
@@ -109,6 +109,52 @@ if assistant_option == "Financial Assistant":
             response = pretty_print(response_messages)
             st.text_area("Response:", value=response, height=300)
 
+
+elif assistant_option == "PDF Analyzer":
+    st.title("PDF Analyzer  :mag:")
+
+    # Description for PDF Analyzer
+    st.markdown("""
+        Use this tool to extract valuable information from PDF documents. Ideal for:
+        - :page_facing_up: **Analyzing text and data** within PDFs for research or business insights.
+        - :mag_right: **Extracting specific information** from large documents quickly.
+        - :clipboard: Converting **PDF content into actionable data** to inform decision-making.
+        - :bookmark_tabs: Gaining insights from **financial reports, research papers, or legal documents** in PDF format.
+
+        Upload a PDF file and enter your specific query related to the document.
+    """)
+
+    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+    user_query = st.text_input("Enter your query about the PDF:")
+
+    if uploaded_file is not None and user_query:
+        with st.spinner('Analyzing PDF...'):
+            temp_dir = "temp"
+            if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)
+
+            temp_file_path = os.path.join(temp_dir, uploaded_file.name)            
+            with open(temp_file_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+
+            try:
+                file_response = client.files.create(
+                    file=open(temp_file_path, "rb"),
+                    purpose="assistants",
+                )
+                assistant = client.beta.assistants.update(
+                    assistant_id= assistant.id,
+                    file_ids=[file_response.id],
+                )
+                thread = client.beta.threads.create()
+                run = submit_message(assistant.id, thread, user_query)
+                run = wait_on_run(run, thread)
+                response_messages = get_response(thread)
+                response = pretty_print(response_messages)
+                st.text_area("Response:", value=response, height=300)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
 # Show a message if the API key is not entered
 if not client:
     st.warning("Please enter your OpenAI API key in the sidebar to use the app.")
